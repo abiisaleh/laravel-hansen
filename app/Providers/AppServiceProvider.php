10948a1377688a8cfca00f051abd33f5776c103a
abiisaleh <abi.saleh.25@gmail.com>
2023-10-31 08:53:37 +0900

    add file manager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +25,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
+
+        FilamentAsset::register([
+            Css::make('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'),
+            Css::make('bootstrap-icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'),
+            Css::make('custom-style', asset('vendor/file-manager/css/file-manager.css')),
+            Js::make('custom-script', asset('vendor/file-manager/js/file-manager.js')),
+        ]);
     }
 }
---
+++ resources/views/filament/pages/file-manager.blade.php
@@ -0,0 +1,6 @@
+
+<x-filament-panels::page>
+<div style="height: 600px;">
+    <div id="fm"></div>
+</div>
+</x-filament-panels::page>
