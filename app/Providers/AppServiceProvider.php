e932ad0bb99d8bc1f2f5f2f3ccc4164e510546cf
abiisaleh <abi.saleh.25@gmail.com>
2023-11-01 10:38:46 +0900

    fix css style

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,11 +26,12 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::unguard();
 
-        FilamentAsset::register([
-            Css::make('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'),
-            Css::make('bootstrap-icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'),
-            Css::make('custom-style', asset('vendor/file-manager/css/file-manager.css')),
-            Js::make('custom-script', asset('vendor/file-manager/js/file-manager.js')),
-        ]);
+        if (str_contains(url()->current(), 'file-manager'))
+            FilamentAsset::register([
+                Css::make('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'),
+                Css::make('bootstrap-icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'),
+                Css::make('custom-style', asset('vendor/file-manager/css/file-manager.css')),
+                Js::make('custom-script', asset('vendor/file-manager/js/file-manager.js')),
+            ]);
     }
 }
