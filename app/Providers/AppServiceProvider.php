e041a833631677898d794f83fd4a2d505f8e5cf8
abiisaleh <abi.saleh.25@gmail.com>
2023-11-02 09:12:25 +0900

    fix btn no background

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
 
         if (str_contains(url()->current(), 'file-manager'))
             FilamentAsset::register([
-                Css::make('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'),
+                Css::make('bootstrap', asset('css/bootstrap.min.css')),
                 Css::make('bootstrap-icon', 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'),
                 Css::make('custom-style', asset('vendor/file-manager/css/file-manager.css')),
                 Js::make('custom-script', asset('vendor/file-manager/js/file-manager.js')),
